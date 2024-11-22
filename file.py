86c697728821ac632db9aa863eba46ff209cd507
minecraft <aloha2k9@gmail.com>
2024-11-22 19:50:24 +0300

    ddd

--- file.py
+++ file.py
@@ -10,26 +10,45 @@ cursor = con.cursor()
 app = Flask(__name__)
 
 
-@app.route('/0')
+@app.route('/')
 def medicines():
     cursor.execute('SELECT * FROM medicines')
     a = cursor.fetchall()
     return render_template('lol.html', medicines=a)
 
 
-@app.route('/')
+@app.route('/0')
 def find():
-    return render_template('gol.html')
 
+    return render_template('get_db.html')
+
+@app.route('/save_db/')
+def save_db():
+    name = request.args.get('name')
+    manufacturer = request.args.get('manufacturer')
+    forma = request.args.get('forma')
+    price = request.args.get('price')
+    id = request.args.get('id')
+    print(name,manufacturer)
+    cursor.execute('INSERT INTO medicines (name,manufacturer,form,price,id) VALUES(?,?,?,?,?)' ,(name,manufacturer,forma,price,id))
+    con.commit()
+    return 'ok'
 
 @app.route('/find_page/')
 def find_page():
-    name = request.args.get('name')
-    cursor.execute('SELECT * FROM medicines WHERE name=(?)', [name])
-    a = cursor.fetchall()
 
+    type_find = request.args.get('type_find')
+    text = request.args.get('name')
+
+    cursor.execute(f'SELECT * FROM medicines WHERE {type_find}=(?)', [text])
+    a = cursor.fetchall()
     print(a)
-    return render_template('ff.html', medicines=a, name=name)
+    return render_template('ff.html', medicines=a)
+
+
+
+
+
 
 
 app.run(debug=True)
